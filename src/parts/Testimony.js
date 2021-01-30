1989c420bf5d5f56f05a620cab34855d96a1e40a
lvstr <serankucing@gmail.com>
2021-01-30 13:56:19 +0700

    Add Star Component

---
+++ src/parts/Testimony.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Star from "elements/Star";
+export default function Testimony() {
+  return (
+    <div>
+      <Star value={5} width={45} height={45} spacing={4}></Star>
+    </div>
+  );
+}

1eac5c1fb920f62fd54d66215375b1457d4c30d0
lvstr <serankucing@gmail.com>
2021-01-31 14:38:52 +0700

    Add InputDate Component

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-import InputNumber from "elements/Form/InputNumber";
+import InputDate from "elements/Form/InputDate";
 
 export default class Example extends Component {
   state = {
-    value: "1",
+    value: {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
   };
+
   handleChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
@@ -19,12 +24,10 @@ export default class Example extends Component {
           style={{ height: "100vh" }}
         >
           <div className="col-auto">
-            <InputNumber
+            <InputDate
               max={30}
-              isSuffixPlural
               onChange={this.handleChange}
               name="value"
-              suffix=" night"
               value={this.state.value}
             />
           </div>

63934ed8e62bfb80ecb74b1a38a7ec93b1afb28c
lvstr <serankucing@gmail.com>
2021-02-01 14:36:57 +0700

    Add Booking Information Part

---
+++ src/parts/Checkout/Payment.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Fade from "react-reveal/Fade";
+
+import { InputText, inputFile } from "elements/Form";
+
+import LogoMandiri from "assets/images/logo/logo_mandiri.png";
+import LogoBca from "assets/images/logo/bca.png";
+
+export default function Payment(props) {
+  return <div></div>;
+}
